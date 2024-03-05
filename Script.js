cec0773ce35a869b6ca16b9c05d660564b47e66a
youssefshawky0 <youssefshawky691@gmail.com>
2024-03-05 14:30:53 +0200

    init

---
+++ Script.js
@@ -0,0 +1,14 @@
+let span = document.getElementById("top");
+
+window.onscroll = function() {
+
+     console.log(this.scrollY);
+
+    if (this.scrollY >= 600) {
+
+        document.getElementById("top").classList.add("show");
+    } 
+    else {
+        document.getElementById("top").classList.remove("show");
+    }
+}
