75cb6bc9e7e14d96b819098ff7051281ba5b064b
youssefshawky0 <youssefshawky691@gmail.com>
2024-05-10 21:15:14 +0300

    fix bugs

--- Script.js
+++ Script.js
@@ -2,12 +2,10 @@ let span = document.getElementById("top");
 
 window.onscroll = function() {
 
-    //  console.log(this.scrollY);
-
     if (this.scrollY >= 600) {
 
         document.getElementById("top").classList.add("show");
-    } 
+    }
     else {
         document.getElementById("top").classList.remove("show");
     }
